20cbad073bba71359ae50667c7257537b2dd160d
joyxu504 <51300591+joyxu504@users.noreply.github.com>
2022-07-20 19:51:34 -0400

    first commit

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+const express = require("express"),
+       app = express(),
+       port = process.env.PORT || 8080,
+       cors = require("cors");
+const bodyParser = require('body-parser');
+const fs = require("fs");
